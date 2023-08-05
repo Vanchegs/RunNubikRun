f15964daf577d7b55a0e54a0c64efcef5502fad5
Vanchegs <manshin9300@gmail.com>
2023-08-05 17:34:41 +0300

    saveUpdate0.1

--- Assets/Scripts/DiamondLogic/DiamondScore.cs
+++ Assets/Scripts/DiamondLogic/DiamondScore.cs
@@ -8,16 +8,44 @@ namespace Vanchegs
     public class DiamondScore : MonoBehaviour
     {
         [SerializeField] private TMP_Text scoreText;
-        private int score = 0;
-        
-        void FixedUpdate()
+        [SerializeField] private TMP_Text highestScoreText;
+        private float score = 0;
+        private float highestScore = 0;
+
+        private void Start()
+        {
+            LoadHighScore();
+        }
+
+        private void FixedUpdate()
         {
             scoreText.text = "" + score;
+            highestScoreText.text = "" + highestScore;
+
+            if (score > highestScore)
+            {
+                highestScore = score;
+                SaveHighScore();
+            }
         }
 
         public void PlusScore()
         {
             score++;
+        }
+
+        private void SaveHighScore()
+        {
+            PlayerPrefs.SetFloat("HighScore", highestScore);
+            PlayerPrefs.Save();
+        }
+
+        private void LoadHighScore()
+        {
+            if (PlayerPrefs.HasKey("HighScore"))
+            {
+                highestScore = PlayerPrefs.GetFloat("HighScore");
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
